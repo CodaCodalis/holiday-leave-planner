dd202b757dd696a325e608e4defe3d19f88ea85d
CodaCodalis <lara.wuxus@gmail.com>
2023-01-05 14:23:17 +0100

    HOPLA-18 add views and templates login, signup, homepage

--- django_project/settings.py
+++ django_project/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.Employee"
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
