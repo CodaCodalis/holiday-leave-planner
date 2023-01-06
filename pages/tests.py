44f3dcc39bcd7a97d0f2458e1708c1edfeb9c581
CodaCodalis <larah.wuxus@gmail.com>
2023-01-06 12:42:26 +0100

    HOPLA-21 add pages app tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(SimpleTestCase):
+    def test_homepage_view_url_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_view(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "Home")
