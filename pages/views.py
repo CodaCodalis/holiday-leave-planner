a2992b3d6db4e9d6df00a3d97b846c356b8f131c
CodaCodalis <larah.wuxus@gmail.com>
2023-01-10 14:47:28 +0100

    HOPLA-37 list logged in user's holiday leaves on homepage

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
+from vacations.models import Vacation
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['vacations'] = Vacation.objects.all()
+        return context
