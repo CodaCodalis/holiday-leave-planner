8307bd9b4fb5d982d7829492df1bf9aec67ce25f
CodaCodalis <lara.wuxus@gmail.com>
2023-01-11 09:05:58 +0100

    HOPLA-98 add team change feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import SignUpView, UserInfoChangeView, UserInfoChangeDoneView
+from .views import SignUpView, UserInfoChangeView, UserInfoChangeDoneView, TeamChangeView, TeamChangeDoneView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("user_info_change/<int:pk>/", UserInfoChangeView.as_view(), name="user_info_change"),
     path("user_info_change_done/", UserInfoChangeDoneView.as_view(), name="user_info_change_done"),
+    path("team_change/<int:pk>/", TeamChangeView.as_view(), name="team_change"),
+    path("team_change_done/", TeamChangeDoneView.as_view(), name="team_change_done"),
 ]
