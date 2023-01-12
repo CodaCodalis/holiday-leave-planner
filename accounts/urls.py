9a868ebb635e757f30f22e35858860ad3352f2e6
CodaCodalis <lara.wuxus@gmail.com>
2023-01-12 10:04:40 +0100

    HOPLA-40 move teams edit to separate page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import SignUpView, UserInfoChangeView, UserInfoChangeDoneView, TeamChangeView, TeamChangeDoneView
+from .views import (SignUpView,
+                    UserInfoChangeView,
+                    UserInfoChangeDoneView,
+                    TeamChangeView,
+                    TeamChangeDoneView,
+                    TeamsEditView)
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("user_info_change/<int:pk>/", UserInfoChangeView.as_view(), name="user_info_change"),
     path("user_info_change_done/", UserInfoChangeDoneView.as_view(), name="user_info_change_done"),
+    path("teams_edit/", TeamsEditView.as_view(), name="teams_edit"),
     path("team_change/<int:pk>/", TeamChangeView.as_view(), name="team_change"),
     path("team_change_done/", TeamChangeDoneView.as_view(), name="team_change_done"),
 ]
