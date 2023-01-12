93b32c3a64992e14db20a55062d00d4ab1170a31
CodaCodalis <lara.wuxus@gmail.com>
2023-01-12 23:03:40 +0100

    HOPLA-36 second shot

--- pages/views.py
+++ pages/views.py
@@ -16,15 +16,13 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # current_user = self.request.user
-        context['teams_with_conflict'] = self.find_conflicts(self.teams, self.users, self.vacations)
+        context['dates_with_conflict'] = self.find_conflicts(self.teams, self.users, self.vacations)
         # context['current_user'] = current_user
         return context
 
     @classmethod
     def find_conflicts(cls, teams, users, vacations):
-        conflict = False
-        teams_with_conflict = list()
-
+        dates_with_conflict = list()
         # für jedes team die anzahl der teammitglieder ermitteln
         for team in teams:
             teammembers = 0
@@ -32,7 +30,7 @@ class HomePageView(TemplateView):
             for user in users:
                 if team == user.team:
                     teammembers += 1
-            # für jeden urlaubseintrag die anzahl der mitglieder ermitteln, die an gleichen tag urlaub genommen haben
+            # für jeden urlaubseintrag die anzahl der mitglieder ermitteln, die am gleichen tag urlaub genommen haben
             vacationers_on_processed_date = 0
             for vacation in vacations:
                 # anzahl der urlaubstage des betrachteten urlaubseintrags ermitteln
@@ -49,9 +47,7 @@ class HomePageView(TemplateView):
                                 compared_processed_date = vacation.start_date + datetime.timedelta(compared_processed_delta)
                                 if processed_date == compared_processed_date:
                                     vacationers_on_processed_date += 1
-                    # if vacationers_on_processed_date > 0 and 1 - (vacationers_on_processed_date / teammembers) < float(min_att) / 100:
-                    if True:
-                        teams_with_conflict.append(team)
-                        break
+                    if vacationers_on_processed_date > 0 and teammembers > 0 and 1 - (vacationers_on_processed_date / teammembers) < float(min_att) / 100:
+                        dates_with_conflict.append(processed_date)
 
-        return teams_with_conflict
+        return dates_with_conflict
