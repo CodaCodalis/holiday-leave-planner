35c9db064a5aadac1f37eda4d1ec71f95478905e
CodaCodalis <larah.wuxus@gmail.com>
2023-01-26 20:45:11 +0100

    BUGFIX pop up messages

--- pages/views.py
+++ pages/views.py
@@ -20,8 +20,9 @@ class HomePageView(LoginRequiredMixin, TemplateView):
         teams = Team.objects.all()
         users = get_user_model().objects.all()
 
+        conflict_dates = list()
         conflicts = list()
-        conflicts_info = {}
+        counter = 0
         for team in teams:
             teammembers = 0
             min_att = float(team.min_attendance) / 100
@@ -35,34 +36,27 @@ class HomePageView(LoginRequiredMixin, TemplateView):
                     processed_date = vacation.start_date + datetime.timedelta(days=processed_delta)
                     vacationers_on_processed_date = 1
                     for compared_vacation in vacations:
-                        vacationers_on_processed_date += cls.get_vacationers(vacation, compared_vacation,
-                                                                             processed_date)
-                if teammembers > 0 and 1 - (vacationers_on_processed_date / teammembers) < min_att:
-                    actual_att = 1 - (vacationers_on_processed_date / teammembers)
-                    if processed_date not in conflicts:
-                        conflicts.append(processed_date)
-                        # conflicts_info[str(processed_date)] = str(user.team)
-                        att = {int(actual_att*100): int(min_att*100)}
-                        team_att = {str(user.team): att}
-                        conflicts_info[processed_date] = team_att
-        return conflicts_info
-
-    @classmethod
-    def get_vacationers(cls, vacation, compared_vacation, processed_date):
-        if not vacation.user == compared_vacation.user:
-            user_team = vacation.user.team
-            compared_user_team = compared_vacation.user.team
-            if user_team == compared_user_team:
-                compared_vacation_delta = compared_vacation.end_date - compared_vacation.start_date
-                compared_vacation_days = compared_vacation_delta.days
-                for compared_processed_delta in range(0, compared_vacation_days + 1):
-                    compared_processed_date = compared_vacation.start_date + datetime.timedelta(
-                        compared_processed_delta)
-                    if processed_date == compared_processed_date:
-                        return 1
-                    else:
-                        return 0
-            else:
-                return 0
-        else:
-            return 0
+                        if not vacation.user == compared_vacation.user:
+                            user_team = vacation.user.team
+                            compared_user_team = compared_vacation.user.team
+                            if user_team == compared_user_team and user.team == team:
+                                compared_vacation_delta = compared_vacation.end_date - compared_vacation.start_date
+                                compared_vacation_days = compared_vacation_delta.days
+                                for compared_processed_delta in range(0, compared_vacation_days + 1):
+                                    compared_processed_date = compared_vacation.start_date + datetime.timedelta(
+                                        days=compared_processed_delta)
+                                    if processed_date == compared_processed_date:
+                                        vacationers_on_processed_date += 1
+                    if teammembers > 0 and 1 - (vacationers_on_processed_date / teammembers) < min_att:
+                        actual_att = 1 - (vacationers_on_processed_date / teammembers)
+                        if processed_date not in conflict_dates:
+                            conflict_dates.append(processed_date)
+                            # conflicts_info[str(processed_date)] = str(user.team)
+                            # infos = [processed_date, str(user.team), int(actual_att*100), int(min_att*100)]
+                            att = {int(actual_att*100): int(min_att*100)}
+                            team_att = {str(user.team): att}
+                            date_team = {processed_date: team_att}
+                            counter += 1
+                            conflict = {str(counter): date_team}
+                            conflicts.append(conflict)
+        return conflicts
