ae18d57ee7e621c24e46131a61bde63462662a68
CodaCodalis <lara.wuxus@gmail.com>
2023-02-17 10:34:40 +0100

    flash messages

--- static/js/pages_homepage.js
+++ static/js/pages_homepage.js
@@ -2,6 +2,7 @@ var conflicts = JSON.parse(conflictsStr);
 var conflictsCount = Object.keys(conflicts).length;
 
 document.getElementById("count").innerText = conflictsCount;
+
 var conflictsNode = document.getElementById('conflicts');
 var conflictNode = document.getElementById('conflict0');
 var conflictSpanDate = document.getElementById("date0");
@@ -19,36 +20,30 @@ conflictSpanAtt.innerText = conflicts[0].att;
 conflictSpanMinAtt.innerText = conflicts[0].min_att;
 
 
-for (var i = 1; i <= conflictsCount; i++) {
-    if (i === conflictsCount) {
-        break;
-    }
-        var conflictNodeAdd = conflictNode.cloneNode(true);
-        conflictNodeAdd.setAttribute("id", "conflict" + [i]);
+for (var i = 0; i < conflictsCount; i++) {
+    console.log(i);
+    var conflictNodeAdd = conflictNode.cloneNode(true);
+    conflictNodeAdd.setAttribute("id", "conflict" + (i+1));
 
-        conflictSpanDate = document.getElementById("date" + [i - 1]);
-        conflictSpanTeam = document.getElementById("team" + [i - 1]);
-        conflictSpanAtt = document.getElementById("att" + [i - 1]);
-        conflictSpanMinAtt = document.getElementById("min_att" + [i - 1]);
+    conflictSpanDate = conflictNodeAdd.querySelector("#date0");
+    conflictSpanTeam = conflictNodeAdd.querySelector("#team0");
+    conflictSpanAtt = conflictNodeAdd.querySelector("#att0");
+    conflictSpanMinAtt = conflictNodeAdd.querySelector("#min_att0");
 
-        conflictSpanDate.setAttribute("id", "date" + [i])
-        conflictSpanTeam.setAttribute("id", "team" + [i])
-        conflictSpanAtt.setAttribute("id", "att" + [i])
-        conflictSpanMinAtt.setAttribute("id", "min_att" + [i])
+    conflictSpanDate.setAttribute("id", "date" + (i+1));
+    conflictSpanTeam.setAttribute("id", "team" + (i+1));
+    conflictSpanAtt.setAttribute("id", "att" + (i+1));
+    conflictSpanMinAtt.setAttribute("id", "min_att" + (i+1));
 
-        unixEpoch = Date.parse(conflicts[i].date);
-        date = new Date(unixEpoch);
-        formattedDate = date.toLocaleDateString();
+    unixEpoch = Date.parse(conflicts[i].date);
+    date = new Date(unixEpoch);
+    formattedDate = date.toLocaleDateString();
 
-        conflictSpanDate.innerText = formattedDate;
-        conflictSpanTeam.innerText = conflicts[i].team;
-        conflictSpanAtt.innerText = conflicts[i].att;
-        conflictSpanMinAtt.innerText = conflicts[i].min_att;
+    conflictSpanDate.innerText = formattedDate;
+    conflictSpanTeam.innerText = conflicts[i].team;
+    conflictSpanAtt.innerText = conflicts[i].att;
+    conflictSpanMinAtt.innerText = conflicts[i].min_att;
 
-        conflictsNode.appendChild(conflictNodeAdd);
+    conflictsNode.appendChild(conflictNodeAdd);
 }
-
-
-
-
-
+document.getElementById("conflict0").remove();
